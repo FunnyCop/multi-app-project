f7ed4b0db9db6888a9b514783b2bbc4c07c5b4b5
Logan Prunty <lb_prunty@outlook.com>
2021-07-20 22:29:30 -0400

    init

--- express-server/server.js
+++ express-server/server.js
@@ -1,27 +1,9 @@
-var express = require('express');
-var app = express();
+const io = require("socket.io");
+const server = io.listen(3000);
 
-var socketio = require('socket.io');
-
-var server = app.listen( 8000, "192.168.1.253", () => {
-    console.log('listening on port 8000');
-});
-
-var io = socketio( server, {pingTimeout: 60000} )
-
-
-io.on( "connection", socket => {
-    console.log( "Client Connected" )
-
-    // socket.emit(
-    //     "Welcome",
-    //     message = `Welcome Client ${ socket.id }`
-    // )
-
-    // socket.broadcast.emit(
-    //     "NewClient",
-    //     message = `Client ${ socket.id } has connected`
-    // )
+server.on( "connection", socket => {
+  console.log("user connected");
+  socket.emit("welcome", "welcome man");
 } )
 
-io.on( "message", data => console.log( 1 ) )
+server.on( "message", data => console.log( 1 ) )
