595f69f8ca0d0afff1ed7ec438f042285babf2b4
Logan Prunty <lb_prunty@outlook.com>
2021-07-20 22:48:17 -0400

    init

--- express-server/server.js
+++ express-server/server.js
@@ -1,11 +1,17 @@
-const httpServer = require("http").createServer();
-const io = require("socket.io")(httpServer)
+const express = require('express');
+const app = express();
 
-io.on( "connection", socket => {
+const server = app.listen(8000, () =>
+  console.log('The server is all fired up on port 8000')
+);
+
+const io = require('socket.io')(server, { cors: true });
+
+io.on("connection", socket => {
   console.log("user connected");
   socket.emit("welcome", "welcome man");
-} )
+})
 
-io.on( "message", data => console.log( 1 ) )
+io.on("message", data => console.log(1))
 
 httpServer.listen(3000);
